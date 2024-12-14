40808464c518fb7f7927ddd929075b52c21ceea7
Johannadpc04 <johanna.dpc@gmail.com>
2024-12-14 15:05:15 -0500

    Update config.js

--- contenedores/miapp/config.js
+++ contenedores/miapp/config.js
@@ -1,8 +1,6 @@
 const config = {
     PORT: 3000,
-    //DB_URL: 'mongodb://dba:12345@mongodb:27017/miapp?authSource=admin'
-    DB_URL: 'mongodb+srv://johannadpc:johanna2004DP@cluster0.yg6h7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    
+    DB_URL: 'mongodb+srv://johannadpc:johanna2004DP@cluster0.yg6h7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'    
 
 }
 
